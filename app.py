4ecb96067e91d9a0402bab88be1afa774d92bae1
Rob-Cortes <rob.collins.cortes@gmail.com>
2023-06-09 18:00:42 -0400

    Now sending complete data through Flask books route

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from psql_connect import get_engine, get_base
 import sqlalchemy
 from sqlalchemy.orm import Session
-import numpy as np
-
-engine = get_engine()
-Base = get_base(engine)
-
-print(Base.classes.keys())
-
-
 from flask import Flask, jsonify
 from flask_cors import CORS
+import numpy as np
 
+# Flask set-up
 app = Flask(__name__)
 CORS(app)
 
+# Sqlalchemy set-up
+engine = get_engine()
+Base = get_base(engine)
+
+# Landing route
 @app.route("/")
 def welcome():
-    """List all available api routes."""
+    # List available api routes
     return(
         f"/api/v1.0/books"
     )
 
+# Best books route
 @app.route("/api/v1.0/books")
 def books():
+    # Start sqlalchemy session
     session = Session(engine)
+
+    # Assign table classes 
     Authors = Base.classes.authors
     Books = Base.classes.books
     Imprints = Base.classes.imprints
@@ -33,14 +36,21 @@ def books():
     Publishers = Base.classes.publishers
     Ownership = Base.classes.ownership
 
-    results = session.query(Results.weeks_on_list, Books.book_title).\
+    # Query SQL database
+    results = session.query(Books.book_title, Books.book_image, Books.book_description, Authors.author_name, Imprints.imprint_name, Publishers.publisher_name, Lists.list_name, Results.weeks_on_list).\
+                        filter(Authors.author_id == Books.author_id).\
+                        filter(Imprints.imprint_id == Books.imprint_id).\
+                        filter(Ownership.imprint_id == Books.imprint_id).\
+                        filter(Publishers.publisher_id == Ownership.publisher_id).\
                         filter(Results.book_id == Books.book_id).\
-                        filter(Results.published_date == '2023-05-21').\
-                        order_by(Results.weeks_on_list.desc()).limit(10).all()
+                        filter(Lists.list_id == Results.list_id).\
+                        order_by(Results.published_date.desc(), Results.weeks_on_list.desc()).all()
+ 
+    # Close sqlalchemy session
     session.close()
-
-    best_books = list(np.ravel(results))
-    return jsonify(best_books)
+    
+    # Return jsonified results
+    return jsonify([dict(_) for _ in results])
 
 if __name__ == '__main__':
     app.run(debug=True)
